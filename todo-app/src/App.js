dde3091808807399c6d3ee1a3772211e8f4dffff
sagar6502 <ssunar@bayonesolutions.com>
2024-01-29 16:01:04 +0530

    Checked

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
+      <ThirdComponent></ThirdComponent>
+      <FourthComponent></FourthComponent>
     </div>
-  );
+  ); 
 }
+
+function FirstComponent() {
+  return (
+    <div className='FirstComponent'>
+      First Component
+    </div>
+  )
+}
+
+function SecondComponent() {
+  return (
+    <div className='SecondComponent'>
+      Second Component
+    </div>
+  )
+}
+
+class ThirdComponent extends Component {
+  render() {
+    return (
+      <div className='ThirdComponent'>
+        Third Component
+      </div>
+    )
+  }
+}
+
+class FourthComponent extends Component {
+  render() {
+    return (
+      <div className='FourthComponent'>
+        Fourth Component
+      </div>
+    )
+  }
+}
+
 
 export default App;
